dd8e24c31136b6de370ae03b01f64501b2d23fba
mh4636 <mh4636@rit.edu>
2022-02-12 14:10:56 -0500

    Added reports.sql for report queries

--- queries/sitemanager.sql
+++ queries/sitemanager.sql
@@ -13,3 +13,24 @@ WHERE siteMngr = "Y" AND accountID = ?;
 
 
 --Query to assign a staff member to one of the sites that the site manager is staffed at.
+
+
+
+
+--Query to set or remove a location's hour of operations
+insert into locationTimes(locationID, locationDay, locationOpen, locationClose) VALUES
+    (?,?,?,?);
+
+DELETE FROM locationTimes WHERE locationID = ? AND locationDay = ?;
+
+UPDATE locationTimes set locationOpen = ?, locationDay = ? WHERE locationID = ? And locationDay = ?
+
+--Query to get hours of operation from a locaiton
+
+SELECT location.locationName, locationDay, locationOpen, locationClose FROM locationTimes
+INNER JOIN location on locationTimes.locationID = location.locationID
+WHERE locationTimes.locationID;
+
+--Query to get all available hours of operations for all locations
+SELECT location.locationName, locationDay, locationOpen, locationClose FROM locationTimes
+INNER JOIN location on locationTimes.locationID = location.locationID;

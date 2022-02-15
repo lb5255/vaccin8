0858093a3265037095283976f24de2229ec17b1f
mh4636 <mh4636@rit.edu>
2022-02-15 13:12:31 -0500

    Trying stuff

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const path = require("path");
 const express = require("express");
+const mysql = require("mysql2");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "student",
+    database: "vaccinationdb"
+});
+
 
 // load the database
 require("./load-db.js");
@@ -8,5 +17,23 @@ const app = express();
 
 // statically serve the client on /
 app.use("/", express.static(path.join(__dirname, "..", "client")));
+app.use(express.json());
+
+app.get("/api/index",(req, res) => {
+    //respond with GET request for all available vaccines.
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT DISTINCT vaccineType, manufacturer FROM campaignvaccines WHERE campaignID IN (SELECT campaignID FROM campaign WHERE campaignStatus = 'a');",
+        function (err, result, fields) {
+            if (err) throw err;
+            res.json();
+        });
+
+    });
+
+});
+
+
+
 
 app.listen(8080, () => console.log("Listening on port 8080"));

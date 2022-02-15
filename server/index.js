fcf29f1288958885b2346733142d67c8da91c005
tim <tnt7864@rit.edu>
2022-02-15 14:47:51 -0500

    adjustments

--- server/index.js
+++ server/index.js
@@ -1,19 +1,8 @@
 const path = require("path");
 const express = require("express");
 
-const mysql = require("mysql2");
-
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "student",
-    database: "vaccinationdb"
-});
-
-
 // load the database
-require("./load-db.js");
+const connProm = require("./load-db.js");
 
 const app = express();
 
@@ -23,18 +12,13 @@ app.use(express.json());
 
 //graphQL for multiple queries
 
-app.get("/api/index",(req, res) => {
+app.get("/api/index", async (req, res) => {
     //respond with GET request for all available vaccines.
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT DISTINCT vaccineType, manufacturer FROM campaignvaccines WHERE campaignID IN (SELECT campaignID FROM campaign WHERE campaignStatus = 'a');",
-        function (err, result, fields) {
-            if (err) throw err;
-            res.json();
-        });
-
-    });
-
+    const conn = await connProm;
+    const [result, _fields] = await conn.execute(
+        "SELECT DISTINCT vaccineType, manufacturer FROM campaignvaccines WHERE campaignID IN (SELECT campaignID FROM campaign WHERE campaignStatus = 'a');"
+    );
+    res.json(result);
 });
 
 app.listen(8080, () => console.log("Listening on port 8080"));

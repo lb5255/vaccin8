f786c966bdd145c39964a12de2af7a9f8dfb0d78
Lea Boyadjian <lb5255@rit.edu>
2022-02-24 15:20:08 -0500

    made cancel modal

--- client/assets/javascript/user.js
+++ client/assets/javascript/user.js
@@ -183,3 +183,31 @@ function createDateTimeResult(data) {
 	
 	id("datetime-results").appendChild(element);
 }
+
+function displayUserCancelModal() {
+	// Get the modal
+	var modal = document.getElementById("cancelAppUserModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("cancelAppUserButton");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+		modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}

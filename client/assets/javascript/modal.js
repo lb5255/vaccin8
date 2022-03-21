6c415dc3deab12d090a87871aa6a862cd5113363
jh5238 <jh5238@rit.edu>
2022-03-21 19:38:03 -0400

    FrontDesk Modal added
    this took forever to figure out lol

---
+++ client/assets/javascript/modal.js
@@ -0,0 +1,11 @@
+function init(){
+
+document.getElementById('checkIn').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
+
+}

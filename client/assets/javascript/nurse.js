3f41c19025b7ab9516d5926e9611154a51633a7c
tim <tnt7864@rit.edu>
2022-03-23 19:59:58 -0400

    you can enter the batch number and revise the vaccine type if needed

--- client/assets/javascript/nurse.js
+++ client/assets/javascript/nurse.js
@@ -1,4 +1,17 @@
-let appts;
+let appt;
+
+apiGet("/api/vaccineList").then(vacc => {
+	const sel = id("vaccine-select-dropdown");
+	
+	// add <option> tags to the select
+	vacc.forEach(v => {
+		sel.appendChild(
+			element("option", {
+				value: v.campaignVaccID,
+			}, `${v.vaccineType} - ${v.manufacturer} - ${v.vaccineDose}`)
+		);
+	});
+});
 
 async function getPatient() {
 	try {
@@ -11,17 +24,48 @@ async function getPatient() {
 		if(!res.length) {
 			return "No patient found";
 		}
+		const patient = res[0];
 		
-		console.log(res);
-		appts = res;
-		id("patient-name").textContent = id("full-name").textContent = res[0].firstName + " " + res[0].lastName;
-		id("address").textContent = res[0].address;
-		id("address2").textContent = res[0].city + ", " + res[0].state + " " + res[0].zip;
-		id("dob").textContent = new Date(res[0].dateOfBirth).toLocaleDateString();
-		id("phone").textContent = res[0].phone || "none";
-		id("email").textContent = res[0].email || "none";
-		id("insur-prov").textContent = res[0].insuranceProvider || "none";
-		id("insur-num").textContent = res[0].insuranceNum || "none";
+		const appts = await apiGet("/api/nurse/appointments", {
+			patientID: patient.patientID
+		});
+		if(!appts.length) {
+			return "No appointment for this patient";
+		}
+		const appointment = appts[0];
+		
+		console.log(patient, appointment)
+		
+		appt = appointment;
+		id("patient-name").textContent = id("full-name").textContent = patient.firstName + " " + patient.lastName;
+		id("address").textContent = patient.address;
+		id("address2").textContent = patient.city + ", " + patient.state + " " + patient.zip;
+		id("dob").textContent = new Date(patient.dateOfBirth).toLocaleDateString();
+		id("phone").textContent = patient.phone || "none";
+		id("email").textContent = patient.email || "none";
+		id("insur-prov").textContent = patient.insuranceProvider || "none";
+		id("insur-num").textContent = patient.insuranceNum || "none";
+		qa(".first-name").forEach(n => n.textContent = patient.firstName);
+		qa(".last-name").forEach(n => n.textContent = patient.lastName);
+		
+		id("vaccine-select-dropdown").value = appt.campaignVaccID;
+		
+	} catch(err) {
+		console.error(err);
+		return "An error occurred";
+	}
+}
+
+async function enterBatch() {
+	try {
+		
+		await apiPost("/api/nurse/appointments", {
+			batchNum: id("batch").value,
+			appointmentID: appt.appointmentID,
+			vaccineID: id("vaccine-select-dropdown").value,
+		}, "PUT", false);
+		
+		toast("Batch number entered!")
 		
 	} catch(err) {
 		console.error(err);
